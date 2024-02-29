859c372fffa984e72ae25bebc9b113e4d5309e37
millken <millken@gmail.com>
2024-02-29 10:32:45 +0800

    add missing sql

--- db/sql/log.go
+++ db/sql/log.go
@@ -8,6 +8,42 @@ import (
 	"github.com/iotexproject/iotex-core/state"
 )
 
+/*
+DROP TABLE IF EXISTS "public"."accounts";
+CREATE TABLE "public"."accounts" (
+
+	"id" serial8 NOT NULL,
+	"block_height" int8 NOT NULL,
+	"account_address" varchar(42) COLLATE "pg_catalog"."default" NOT NULL DEFAULT ''::character varying,
+	"balance" numeric(42,0) NOT NULL DEFAULT 0,
+	"voting_weight" numeric(42,0) NOT NULL DEFAULT 0,
+	"is_contract" bool NOT NULL DEFAULT false,
+	"is_candidate" bool NOT NULL DEFAULT false
+
+)
+;
+ALTER TABLE "public"."accounts" OWNER TO "postgres";
+
+-- ----------------------------
+-- Indexes structure for table accounts
+-- ----------------------------
+CREATE UNIQUE INDEX "accounts_block_height_account_address_idx" ON "public"."accounts" USING btree (
+
+	"block_height" "pg_catalog"."int8_ops" ASC NULLS LAST,
+	"account_address" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
+
+);
+CREATE INDEX "accounts_block_height_idx" ON "public"."accounts" USING btree (
+
+	"block_height" "pg_catalog"."int8_ops" ASC NULLS LAST
+
+);
+
+-- ----------------------------
+-- Primary Key structure for table accounts
+-- ----------------------------
+ALTER TABLE "public"."accounts" ADD CONSTRAINT "account_balance_pkey" PRIMARY KEY ("id");
+*/
 func StoreAccount(sm protocol.StateManager, addr address.Address, account *state.Account) error {
 	if !dbOpened {
 		return nil
